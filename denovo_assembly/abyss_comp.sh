300844ab62bf1f263f935786a8472a98c324ddba
Rowena-h <rowena.c.hill@gmail.com>
2021-12-08 16:46:29 +0000

    fix sorting

--- denovo_assembly/abyss_comp.sh
+++ denovo_assembly/abyss_comp.sh
@@ -10,7 +10,7 @@ do
 
 	abyss-fac abyss/${STRAIN}/k*/${STRAIN}-contigs.fa > abyss/${STRAIN}_kcomp
 
-	KMER=$(tail -n +2 abyss/${STRAIN}_kcomp | sort -r -k 6 | awk '{print $11}' | sed "s#abyss/${STRAIN}/##" | sed "s#/${STRAIN}-contigs.fa##" | head -n +1)
+	KMER=$(tail -n +2 abyss/${STRAIN}_kcomp | sort -n -r -k 6 | awk '{print $11}' | sed "s#abyss/${STRAIN}/##" | sed "s#/${STRAIN}-contigs.fa##" | head -n +1)
 
 	echo "${KMER} selected" >> abyss/${STRAIN}_kcomp
 
