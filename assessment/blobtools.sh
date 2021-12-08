3b1e73ee049d0d8d2e83301d819d111114d1031c
Rowena-h <rowena.c.hill@gmail.com>
2021-12-08 16:51:12 +0000

    change taxonomic rank

--- assessment/blobtools.sh
+++ assessment/blobtools.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 #$ -cwd           	# Set the working directory for the job to the current directory
 #$ -pe smp 1 		# Request 1 core
@@ -6,7 +7,7 @@
 #$ -j y
 #$ -m bea
 
-STRAIN=$(cat ../strains_shortread ../strains_hybrid | sed -n ${SGE_TASK_ID}p)
+STRAIN=$(cat ../strains_shortread ../strains_hybrid | sed -n ${SGE_TASK_ID}p | awk '{print $1}')
 
 module load samtools
 module load anaconda3
@@ -17,35 +18,45 @@ mkdir ${STRAIN}/blobtools
 if grep -Fq ${STRAIN} ../strains_shortread
 then
 
-	samtools index ../spades/${STRAIN}/bwa/${STRAIN}_spades_mapped_sorted_dups.bam
+	samtools index ${STRAIN}/${STRAIN}_spades_srmapped_coordinatesorted.bam
 
 	~/Programmes/blobtools/blobtools create -i ../denovo_assembly/spades/${STRAIN}/${STRAIN}_spades_polished_filtered.fa \
-						-b ../denovo_assembly/spades/${STRAIN}/bwa/${STRAIN}_spades_mapped_sorted_dups.bam \
+						-b ${STRAIN}/${STRAIN}_spades_srmapped_coordinatesorted.bam \
 						-t ${STRAIN}/${STRAIN}_spades_blast.tsv \
 						-o ${STRAIN}/blobtools/${STRAIN}_spades_blobtools
-
-	for i in species family phylum
+	
+	for i in order
 	do
+
+		~/Programmes/blobtools/blobtools view  -i ${STRAIN}/blobtools/${STRAIN}_spades_blobtools.blobDB.json \
+ 	                                               -o ${STRAIN}/blobtools/${i} \
+ 	                                               --rank $i
+
 		~/Programmes/blobtools/blobtools plot 	-r $i \
 							-i ${STRAIN}/blobtools/${STRAIN}_spades_blobtools.blobDB.json \
-							-o ${STRAIN}/blobtools/${STRAIN}_spades_blobtools
+							-o ${STRAIN}/blobtools/
 	done
 
 elif grep -Fq ${STRAIN} ../strains_hybrid
 then
 	
-	 samtools index ../raven/${STRAIN}/${STRAIN}_raven_mapped_sorted_dups.bam
+	samtools index ${STRAIN}/${STRAIN}_raven_lrmapped_coordinatesorted.bam
 
-        ~/Programmes/blobtools/blobtools create         -i ../denovo_assembly/raven/${STRAIN}/${STRAIN}_raven_polished_filtered.fa
-                                                        -b ../denovo_assembly/raven/${STRAIN}/${STRAIN}_raven_mapped_sorted_dups.bam
-                                                        -t ${STRAIN}/${STRAIN}_raven_blast.tsv \
+        ~/Programmes/blobtools/blobtools create         -i ../denovo_assembly/raven/${STRAIN}/${STRAIN}_raven_polished_filtered.fa \
+                                                        -b ${STRAIN}/${STRAIN}_raven_lrmapped_coordinatesorted.bam \
+                                                       	-t ${STRAIN}/${STRAIN}_raven_blast.tsv \
                                                         -o ${STRAIN}/blobtools/${STRAIN}_raven_blobtools
 
-	for i in species family phylum
+	for i in order
         do
                 ~/Programmes/blobtools/blobtools plot   -r $i \
                                                         -i ${STRAIN}/blobtools/${STRAIN}_raven_blobtools.blobDB.json \
-                                                        -o ${STRAIN}/blobtools/${STRAIN}_raven_blobtools
+                                                        -o ${STRAIN}/blobtools/
+		
+		~/Programmes/blobtools/blobtools view	-i ${STRAIN}/blobtools/${STRAIN}_raven_blobtools.blobDB.json \
+                                                	-o ${STRAIN}/blobtools/${i} \
+	                                                --rank $i
+
         done
 
 fi
