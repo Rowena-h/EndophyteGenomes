77198ce9955533ef28a5c367407db3b56783712b
Rowena-h <rowena.c.hill@gmail.com>
2022-09-01 13:09:59 +0100

    update example one-liners

--- phylogenetics/file_prep.sh
+++ phylogenetics/file_prep.sh
@@ -1,7 +1,9 @@
-cat GenePull/${LINEAGE}/*${MARKER}.fa alignments/${LINEAGE}/${MARKER}.fasta > tmp && mv tmp alignments/${LINEAGE}/${MARKER}.fasta
+#!/bin/sh
 
-sed 's/>[^ ]* />/' GAPDH.fasta | sed 's/glyce.*//' | sed 's/GAPDH.*//' > tmp && mv tmp GAPDH.fasta
-sed 's/>[^ ]* />/' HIS3.fasta | sed 's/hist.*//' | sed 's/HIS.*//' > tmp && mv tmp HIS3.fasta
-sed 's/>[^ ]* />/' TUB2.fasta | sed 's/beta.*//' > tmp && mv tmp TUB2.fasta
+#Example one-liners to prepare fasta sequence headers in gene alignments to be identical prior to concatenation
 
-sed 's/ /_/g' GAPDH.fasta | sed 's/_$//' | sed 's/:/_/' | sed 's/(//' | sed 's/)//' > tmp && mv tmp GAPDH.fasta
+#Removes accession and everything after key identifiers (e.g. name and voucher) based on certain words
+sed 's/>[^ ]* />/' gene_alignment.fasta | sed 's/ glyce.*//' | sed 's/ GAPDH.*//' > tmp && mv tmp gene_alignment.fasta
+
+#Replaces spaces, colons and brackets with underscores
+sed 's/ /_/g' gene_alignment.fasta | sed 's/_$//' | sed 's/:/_/' | sed 's/(//' | sed 's/)//' > tmp && mv tmp gene_alignment.fasta
