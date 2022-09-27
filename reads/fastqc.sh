571e89aa299a2ff2a158dc0a33d778f9cb233eb5
Rowena-h <rowena.c.hill@gmail.com>
2022-09-27 16:20:58 +0100

    update strain variable

--- reads/fastqc.sh
+++ reads/fastqc.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 #$ -cwd           # Set the working directory for the job to the current directory
-#$ -pe smp 4      # Request 4 cores
-#$ -l h_rt=24:0:0 # Request 24 hour runtime
+#$ -pe smp 1      # Request 4 cores
+#$ -l h_rt=1:0:0 # Request 24 hour runtime
 #$ -l h_vmem=1G   # Request 1GB RAM
 #$ -j y
-#$ -t 1-15
 
-STRAIN=$(sed -n ${SGE_TASK_ID}p ../strains_shortread)
+STRAIN=$(sed -n ${SGE_TASK_ID}p ../strains_shortread | awk '{print $1}')
 
 module load fastqc
 
