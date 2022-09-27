3a25cb99b94e38c195fb90d62aa24bf666cbab23
Rowena-h <rowena.c.hill@gmail.com>
2022-09-27 16:34:18 +0100

    add coverage filtering

--- assessment/remove_contam.sh
+++ assessment/remove_contam.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
 #$ -cwd                 # Set the working directory for the job to the current directory
-#$ -pe smp 1            # Request 5 cores
-#$ -l h_rt=1:00:00     # Request 48 hours runtime
-#$ -l h_vmem=1G         # Request 2GB RAM
+#$ -pe smp 1            # Request 1 core
+#$ -l h_rt=1:00:00     	# Request 1 hour runtime
+#$ -l h_vmem=1G         # Request 1GB RAM
 #$ -j y
 
 STRAIN=$(cat ../strains_shortread ../strains_hybrid | sed -n ${SGE_TASK_ID}p | awk '{print $1}')
+ASSEMBLER=$(cat ../strains_shortread ../strains_hybrid | sed -n ${SGE_TASK_ID}p | awk '{print $4}')
 CLASS=$(cat ../strains_shortread ../strains_hybrid | sed -n ${SGE_TASK_ID}p | awk '{print $3}')
 
 module load seqtk
 
-if grep -Fq ${STRAIN} ../strains_shortread
-then
-	awk 'NR==FNR {A[$1]++;next} $6 in A {print $1}' orders_${CLASS} ${STRAIN}/blobtools/order.${STRAIN}_spades_blobtools_uniref_nt.blobDB.bestsum.table.txt | \
-	seqtk subseq ../denovo_assembly/spades/${STRAIN}/${STRAIN}_spades_polished_filtered.fa - > ${STRAIN}/blobtools/${STRAIN}_spades_polished_filtered_nocontam.fa
+#Filter by taxonomy (same class)
 
-elif grep -Fq ${STRAIN} ../strains_hybrid
-then
-	
-	ASSEMBLER=$(cat ../strains_shortread ../strains_hybrid | sed -n ${SGE_TASK_ID}p | awk '{print $4}')
+awk 'NR==FNR {A[$1]++;next} $6 in A {print $1}' orders_${CLASS} ${STRAIN}/blobtools/order.${STRAIN}_${ASSEMBLER}_blobtools_uniref_nt.blobDB.bestsum.table.txt | \
+seqtk subseq ../denovo_assembly/${ASSEMBLER}/${STRAIN}/${STRAIN}_${ASSEMBLER}_polished_filtered.fa - > ${STRAIN}/blobtools/${STRAIN}_${ASSEMBLER}_polished_filtered_nocontam.fa
 
-	awk 'NR==FNR {A[$1]++;next} $6 in A {print $1}' orders_${CLASS} ${STRAIN}/blobtools/order.${STRAIN}_${ASSEMBLER}_blobtools_uniref_nt.blobDB.bestsum.table.txt | \
-	seqtk subseq ../denovo_assembly/${ASSEMBLER}/${STRAIN}/${STRAIN}_${ASSEMBLER}_polished_filtered.fa - > ${STRAIN}/blobtools/${STRAIN}_${ASSEMBLER}_polished_filtered_nocontam.fa
-fi
+#Filter by coverage (>10)
+
+sed '/^#/d' ${STRAIN}/blobtools/order.${STRAIN}_${ASSEMBLER}_blobtools_uniref_nt.blobDB.bestsum.table.txt | awk '{if($5>10)print $1}' | \
+seqtk subseq ${STRAIN}/blobtools/${STRAIN}_${ASSEMBLER}_polished_filtered_nocontam.fa - > ${STRAIN}/blobtools/${STRAIN}tmp && mv ${STRAIN}/blobtools/${STRAIN}tmp ${STRAIN}/blobtools/${STRAIN}_${ASSEMBLER}_polished_filtered_nocontam.fa
