44d8bac4aae4bdc0fa47ff84498d30983788fb5e
Rowena-h <rowena.c.hill@gmail.com>
2022-09-27 16:23:43 +0100

    add cleanup option

--- ncbi_upload/table2asn.sh
+++ ncbi_upload/table2asn.sh
@@ -6,21 +6,22 @@
 #$ -t 1
 
 STRAIN=$(cat ../strains_shortread ../strains_hybrid | awk '{print $1}' | sed -n ${SGE_TASK_ID}p)
-NAME=$(grep ${STRAIN} ../annotation/strain_info | awk -F'\t' '{print $2}')
-NAME2=$(grep ${STRAIN} ../annotation/strain_info | awk -F'\t' '{print $2}' | sed 's/ /_/g')
+NAME=$(grep ${STRAIN} ../annotation/funannotate/strain_info | awk -F'\t' '{print $2}')
+NAME2=$(grep ${STRAIN} ../annotation/funannotate/strain_info | awk -F'\t' '{print $2}' | sed 's/ /_/g')
 ASSEMBLER=$(cat ../strains_shortread ../strains_hybrid | awk '{print $4}' | sed -n ${SGE_TASK_ID}p)
-LOCUS_TAG=$(grep ${STRAIN} ../annotation/strain_info | awk -F'\t' '{print $3}')
+LOCUS_TAG=$(grep ${STRAIN} ../annotation/funannotate/strain_info | awk -F'\t' '{print $3}')
 
 mkdir ${STRAIN}
 
-~/Programmes/table2asn 	-i ../annotation/funannotate/${STRAIN}_gag/${STRAIN}.fasta \
+~/Programmes/table2asn 	-i ../annotation/funannotate/${STRAIN}/predict_results/${NAME2}_IMI${STRAIN}.scaffolds.fa \
 			-outdir ${STRAIN} \
-			-f ../annotation/funannotate/${STRAIN}_gag/${STRAIN}.gff \
+			-f ../annotation/funannotate/${STRAIN}//predict_results/${NAME2}_IMI${STRAIN}.gff3 \
 			-j "[organism=${NAME}][strain=IMI${STRAIN}]" \
 			-t template_${STRAIN}.sbt \
 			-locus-tag-prefix ${LOCUS_TAG} \
+			-c w \
 			-M n \
 			-Z \
 			-euk
 
-cp ${STRAIN}/${STRAIN}.sqn aspera_upload
+cp ${STRAIN}/${NAME2}_IMI${STRAIN}.scaffolds.sqn aspera_upload/${STRAIN}.sqn
