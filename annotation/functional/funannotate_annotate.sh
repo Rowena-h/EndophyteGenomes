ddbacd28fb950bdd19cd160c4b3df06929158a97
Rowena-h <rowena.c.hill@gmail.com>
2022-10-19 15:37:10 +0100

    clean up

--- annotation/functional/funannotate_annotate.sh
+++ annotation/functional/funannotate_annotate.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 #$ -cwd           	# Set the working directory for the job to the current directory
 #$ -pe smp 5		# Request 5 cores
-#$ -l h_rt=240:00:0 	# Request 240 hours runtime
-#$ -l h_vmem=60G   	# Request 1GB RAM
+#$ -l h_rt=12:00:0 	# Request 12 hours runtime
+#$ -l h_vmem=60G   	# Request 60GB RAM
 #$ -l highmem
 #$ -j y
 #$ -m ea
 
 STRAIN=$(cat ../../strains_shortread ../../strains_hybrid | awk '{print $1}' | sed -n ${SGE_TASK_ID}p)
-ASSEMBLER=$(cat ../../strains_shortread ../../strains_hybrid | awk '{print $4}' | sed -n ${SGE_TASK_ID}p)
 
 NAME=$(grep ${STRAIN} ../structural/strain_info | awk -F'\t' '{print $2}')
 NAME2=$(grep ${STRAIN} ../structural/strain_info | awk -F'\t' '{print $2}' | sed 's/ /_/g')
@@ -20,6 +19,6 @@ NAME2=$(grep ${STRAIN} ../structural/strain_info | awk -F'\t' '{print $2}' | sed
 						--eggnog eggnog-mapper/${STRAIN}_proteins.emapper.annotations \
 						--iprscan interproscan/${STRAIN}_proteins_interproscan.xml \
 						--antismash antismash/${STRAIN}/${STRAIN}_proteins.gbk \
-						--sbt ../../ncbi_upload/template_${STRAIN}.sbt \
+						--sbt template_${STRAIN}.sbt \
 						-o ${STRAIN}_functionalannotation \
 						--cpus ${NSLOTS}
