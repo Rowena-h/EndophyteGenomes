1d250340f8dc72990cc189951a049fb055a00f11
Rowena-h <rowena.c.hill@gmail.com>
2022-10-19 15:35:33 +0100

    fix version

--- annotation/functional/interproscan.sh
+++ annotation/functional/interproscan.sh
@@ -11,7 +11,7 @@ NAME=$(grep ${STRAIN} ../structural/strain_info | awk -F'\t' '{print $2}' | sed 
 
 module load java/11.0.2
 
-/data/scratch/btx494/interproscan-5.59-91.0/interproscan.sh \
+/data/scratch/btx494/interproscan-5.57-90.0/interproscan.sh \
 	-i /data/SBCS-BuggsLab/RowenaHill/genome_assemblies/assembly_pipeline/annotation/structural/${STRAIN}/predict_results/${NAME}_IMI${STRAIN}.proteins.fa \
 	-o interproscan/${STRAIN}_proteins_interproscan.xml \
 	-appl CDD,COILS,Gene3D,HAMAP,MobiDBLite,PANTHER,Pfam,Phobius,PIRSF,PRINTS,SignalP_EUK,SFLD,SMART,SUPERFAMILY,TIGRFAM \
